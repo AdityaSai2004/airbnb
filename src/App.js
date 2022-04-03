d7893b2bdc673d0e9f341c9161d2ac0950bc2f63
AdityaSai2004 <2004sai@gmail.com>
2022-04-03 13:50:59 +0530

    no change pic

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React from "react";
 import Navbar from "./navbar";
 import Hero from "./hero";
 import Card from "./Card";
+import data from "./data";
 export default function App() {
+      const cards = data.map(item => {
+        return (
+            <Card 
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })        
+
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card 
-                
-                rating="5.0"
-                reviewCount={6}
-                country="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            />
+      {cards}
     </div>
   );
 }
